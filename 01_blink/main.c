528ed76c5535bc5435c79bf6d50644f733963a19
gkielian <gkielian@google.com>
2015-10-07 16:33:19 -0700

    add pwm, add pwm Makefile, clean blink main.c

--- 01_blink/main.c
+++ 01_blink/main.c
@@ -1,19 +1,22 @@
-#define F_CPU 16000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void) {
-  DDRB = 0b00001000;
+  DDRB |= (1 << DDB3);
 
-  while (1) {
-    PORTB = 0x00; _delay_ms(100);
-    PORTB = 0b00001000; _delay_ms(100);
-  }
-  /*
+  //effectively above is
+  //DDRB = 0b00001000;
+  
   //alternate blink
   while (1) {
-    PORTB &= ~(1 << PB3); _delay_ms(100);
-    PORTB |= (1 << PB3); _delay_ms(100);
+    PORTB &= ~(1 << PB3); _delay_ms(1000);
+    PORTB |= (1 << PB3); _delay_ms(1000);
   }
-  */
+
+  //effectively above is
+  //  while (1) {
+  //    PORTB = 0b00001000; _delay_ms(1000);
+  //    PORTB = 0b00000000; _delay_ms(1000);
+  //  }
+  
 }
